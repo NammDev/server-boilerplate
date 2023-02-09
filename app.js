7fd74bcd6ead6e584f687b7372ea28f4fa2c8d75
Nam <namdeveloper.ca@gmail.com>
2023-02-09 11:16:51 +0700

    Fix async of sequelize connect

--- app.js
+++ app.js
@@ -5,19 +5,22 @@ const config = require('./config')[process.env.NODE_ENV || 'development']
 
 // Connect DB
 const { Sequelize } = require('sequelize')
+const sequelize = new Sequelize(config.postgres.options)
 
-async function connectDb() {
-  const sequelize = new Sequelize(config.postgres.options) // Example for postgres
-  try {
-    await sequelize.authenticate()
-    console.log('Connection has been established successfully.')
-    return sequelize
-  } catch (error) {
-    console.error('Unable to connect to the database:', error)
-  }
+function connectDb() {
+  sequelize
+    .authenticate()
+    .then(() => {
+      console.log('Connection has been established successfully.')
+    })
+    .catch((error) => {
+      console.error('Unable to connect to the database:', error)
+    })
+  return sequelize
 }
 
-config.postgres.client = connectDb()
+const postgresClient = connectDb()
+config.postgres.client = postgresClient
 
 // Routes
 const routes = require('./routes')
