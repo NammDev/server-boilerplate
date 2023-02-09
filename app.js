d3abee7c8bb6e88119c77cb5da130122553bb3cc
Nam <namdeveloper.ca@gmail.com>
2023-02-09 10:25:27 +0700

    setup express

---
+++ app.js
@@ -0,0 +1,13 @@
+// Setup
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+// Listen
+port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
