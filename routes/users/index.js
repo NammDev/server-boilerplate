7fd6bb7bfe4aa6690804d4a1b6cff3f0b7719cb5
Nam <namdeveloper.ca@gmail.com>
2023-02-09 15:28:52 +0700

    CRUD with database

--- routes/users/index.js
+++ routes/users/index.js
@@ -4,12 +4,49 @@ const router = express.Router()
 
 module.exports = (config) => {
   const userService = new UserService(config.postgres.client)
+
+  router.post('/create', async (req, res, next) => {
+    try {
+      const user = await userService.createUser(req.body)
+      res.send(user)
+    } catch (err) {
+      return next(err)
+    }
+  })
+
+  router.get('/all', async (req, res, next) => {
+    try {
+      const userList = await userService.getAllUsers()
+      res.send(userList)
+    } catch (err) {
+      return next(err)
+    }
+  })
+
+  router.get('/all/email', async (req, res, next) => {
+    try {
+      const userList = await userService.getAllUsersAttribute()
+      res.send(userList)
+    } catch (err) {
+      return next(err)
+    }
+  })
+
+  router.get('/findone', async (req, res, next) => {
+    try {
+      const user = await userService.findOneUser()
+      res.send(user)
+    } catch (err) {
+      return next(err)
+    }
+  })
+
   router.get('/', async (req, res) => {
     try {
       const user = await userService.getUser()
       res.json(user)
-    } catch (error) {
-      console.log(error)
+    } catch (err) {
+      return next(err)
     }
   })
   return router
--- services/UserService.js
+++ services/UserService.js
@@ -1,10 +1,88 @@
 const Models = require('../models/sequelize')
+// CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 class UserService {
   constructor(sequelize) {
     Models(sequelize)
     this.client = sequelize
     this.models = sequelize.models
+  }
+
+  async createUser({ firstName, lastName, email, password }) {
+    try {
+      const user = await this.models.User.create({
+        firstName,
+        lastName,
+        email,
+        password,
+      })
+      return user
+    } catch (error) {
+      return error
+    }
+  }
+
+  async getAllUsers() {
+    try {
+      const users = await this.models.User.findAll()
+      return users
+    } catch (err) {
+      return err
+    }
+  }
+
+  async getAllUsersAttribute() {
+    try {
+      const users = await this.models.User.findAll({
+        attributes: ['firstName', 'email'],
+        // attributes: { exlude: ['password']}
+      })
+      return users
+    } catch (err) {
+      return err
+    }
+  }
+
+  // Return only 1 record
+  async findOneUser() {
+    try {
+      const users = await this.models.User.findOne({
+        where: { firstName: 'Nguyen' },
+      })
+      return users
+    } catch (err) {
+      return err
+    }
+  }
+
+  async getAllUsersWhere() {
+    try {
+      const users = await this.models.User.findAll({ where: { firstName: 'Nguyen' } })
+      return users
+    } catch (err) {
+      return err
+    }
+  }
+
+  async updateUser() {
+    try {
+      await this.models.User.update(
+        { lastName: 'lastName changed' },
+        { where: { firstName: 'wdj' } }
+      )
+      return 'updated User'
+    } catch (err) {
+      return err
+    }
+  }
+
+  async deleteUser() {
+    try {
+      const user = await this.models.User.destroy({ where: { firstName: 'wdj' } })
+      return 'deleted User'
+    } catch (err) {
+      return err
+    }
   }
 
   async getUser() {
