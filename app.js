5874aa10dd21ffa6cd8ba1d1cb97e1150ce7fecd
Nam <namdeveloper.ca@gmail.com>
2023-02-09 10:55:22 +0700

    Connect sequelize & db

--- app.js
+++ app.js
@@ -2,6 +2,22 @@
 const express = require('express')
 const app = express()
 
+// Connect DB
+const { Sequelize } = require('sequelize')
+
+async function connectDb() {
+  const sequelize = new Sequelize('postgres://postgres:namkhanh@localhost:5432/phongtro') // Example for postgres
+  try {
+    await sequelize.authenticate()
+    console.log('Connection has been established successfully.')
+    return sequelize
+  } catch (error) {
+    console.error('Unable to connect to the database:', error)
+  }
+}
+
+connectDb()
+
 // Routes
 const routes = require('./routes')
 // Serices
